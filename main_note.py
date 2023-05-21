7df64b4ab9d946a27bf92536dffa5e3894c0499d
GBeretta01 <grejomr2002@gmail.com>
2023-05-21 16:43:24 -0400

    The options to create, show and delete are working

--- main_note.py
+++ main_note.py
@@ -35,27 +35,61 @@ def read_notes():
 
 def show_note():
 
-    print("4- Volver")
+    print("x- Volver")
     opc = input("Elija el nº de la nota a mostrar: ")
 
     with open("notas.csv", "r") as doc:
         read_csv = csv.reader(doc)
         linea = list(read_csv)
 
-        if opc == "4":
-            return
-        elif int(opc)<1 or int(opc)>len(linea):
-            print("Seleccione una opción válida.")
+        if opc.lower() == "x":
             return
         
-        linea_chose = linea[int(opc)-1]
-        title = linea_chose [0]
-        content = linea_chose[1]
-        date = linea_chose[2]
+        try:
+            if int(opc)<1 or int(opc)>len(linea):
+                print("Seleccione una opción válida.")
+                return
+            
+            linea_chose = linea[int(opc)-1]
+            title = linea_chose [0]
+            content = linea_chose[1]
+            date = linea_chose[2]
 
-        print(f"Título: {title}")
-        print(f"Nota: {content}")
-        print(f"Fecha: {date}")
+            print(f"Título: {title}")
+            print(f"Nota: {content}")
+            print(f"Fecha de creación: {date}")
+        
+        except ValueError:
+            print("Ingrese una opción válida (número) o 'x' para volver.")
+
+def delete_note():
+
+    print("X- Volver")
+    opc = input("Elije una opción a borrar: ")
+
+
+    lines = []
+    with open("notas.csv", "r") as doc:
+        reader_notes = csv.reader(doc)
+        lines = list(reader_notes)
+
+    if opc.lower() == "x":
+        return
+    
+    try:
+
+        if int(opc)<1 or int(opc)>len(lines):
+            print("Escoja una opción válida...")
+            return
+        
+        lines.pop(int(opc) - 1)
+
+        with open("notas.csv", "w", newline="") as doc_w:
+            rewriter_csv = csv.writer(doc_w)
+            rewriter_csv.writerows(lines)
+
+    except ValueError:
+        print("Ingrese una opción válida (número) o 'x' para volver.") 
 
 
 # Programa principal
@@ -76,7 +110,8 @@ while True:
         print("In progress...")
         input("Presione [ENTER] para continuar...")
     elif answ == "4":
-        print("In progress...")
+        read_notes()
+        delete_note()
         input("Presione [ENTER] para continuar...")
     else:
         print("Agregue una opción válida...")
